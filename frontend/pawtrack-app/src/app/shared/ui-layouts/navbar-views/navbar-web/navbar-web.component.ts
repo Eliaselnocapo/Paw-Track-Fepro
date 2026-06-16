5920312bb812d78b8c059aa6ac5f747294189014
KiBouToujou <evemor40@gmail.com>
2026-06-16 02:11:23 -0600

    Ajusta navbar responsivo y menu lateral

--- frontend/pawtrack-app/src/app/shared/ui-layouts/navbar-views/navbar-web/navbar-web.component.ts
+++ frontend/pawtrack-app/src/app/shared/ui-layouts/navbar-views/navbar-web/navbar-web.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MenuToggleComponent } from '../../menu-toggle/menu-toggle.component';
 
 @Component({
   selector: 'app-navbar-web',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive], // Importante agregar RouterLinkActive
+  imports: [RouterLink, RouterLinkActive, MenuToggleComponent], // Importante agregar RouterLinkActive
   templateUrl: './navbar-web.component.html',
   styleUrls: ['./navbar-web.component.scss']
 })
 export class NavbarWebComponent {
   // TODO: Esto cambiará dinámicamente cuando el login con el backend esté listo
-  isLoggedIn: boolean = false; 
+  isLoggedIn: boolean = false;
 }
