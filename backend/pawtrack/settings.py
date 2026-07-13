ad11a5d24cd48f1926a367a32ff8840d075cea5f
Manuel <manuelmontespioquinto@gmail.com>
2026-07-12 23:21:27 -0600

    WIP: guardando cambios antes de traer develop

--- backend/pawtrack/settings.py
+++ backend/pawtrack/settings.py
@@ -220,3 +220,12 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 ACCOUNT_LOGIN_METHODS = {'email'}
 ACCOUNT_SIGNUP_FIELDS = ['email*', 'password1*', 'password2*']
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://redis:6379/1', 
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
