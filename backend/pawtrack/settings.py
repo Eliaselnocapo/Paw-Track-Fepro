d024545f1a7fc52b95c99a99f3de51f27a71416f
Manuel <manuelmontespioquinto@gmail.com>
2026-08-06 17:03:47 -0600

    feat: fix de Google Auth y roles por defecto

--- backend/pawtrack/settings.py
+++ backend/pawtrack/settings.py
@@ -153,6 +153,7 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 
+SOCIALACCOUNT_ADAPTER = 'bd.adapters.CustomSocialAccountAdapter'
 SOCIALACCOUNT_EMAIL_VERIFICATION = 'none'
 SOCIALACCOUNT_PROVIDERS = {
     'google': {
